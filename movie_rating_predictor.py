94368dba952ec5271ae973887afc9d8afd303891
Karthik Chandrasekar <karthidec1@gmail.com>
2013-11-24 17:02:01 -0700

    Getting output for movie rating prediction, user based

--- movie_rating_predictor.py
+++ movie_rating_predictor.py
@@ -1,6 +1,4 @@
-import os, codecs
-import numpy, sys
-import operator
+import os, codecs, math, numpy, sys, operator
 
 class movie_predictor:
 
@@ -10,10 +8,14 @@ class movie_predictor:
         self.input_file = os.path.join(self.input_dir, self.file_name)
 
     def run_main(self):
+        user_based_pred = 0
+        item_based_pred = 0
+
         self.load_matrix()
         self.get_input_values()
-        self.user_based()
- 
+        user_based_pred = self.user_based()
+        print user_based_pred 
+
     def load_matrix(self):
         self.initialize_matrix()
         self.open_file()
@@ -21,23 +23,27 @@ class movie_predictor:
         self.close_file()
 
     def get_input_values(self):
-        self.n_size = sys.argv[0]
-        self.user_id = sys.argv[1]
-        self.item_id = sys.argv[2]
+        self.n_size = int(sys.argv[1])
+        self.user_id = int(sys.argv[2])
+        self.item_id = int(sys.argv[3])
 
-    def user_based(self)
+    def user_based(self):
+
+        num = user_avg = deno = 0
+
         self.n_to_sim_dict = self.find_n_similar_neighbours()
+
         self.n_to_avg_rat_dict = self.find_user_rating_avg()
-        
-        self.user_avg = sum(self.user_rat_list)/float(len(self.user_rat_list))
-        self.deno = sum(self.n_to_sim_dict.values())
+
+        user_avg = sum(self.user_rat_list)/float(len(self.user_rat_list))
+        deno = sum(self.n_to_sim_dict.values())
 
         for user, sim in self.n_to_sim_dict.iteritems():
             rat_avg = self.n_to_avg_rat_dict[user]
             rate = self.user_item_matrix[user][self.item_id]
             num += sim * (rate-rat_avg)
                       
-        return self.user_avg + (num/float(self.deno))
+        return user_avg + (num/float(deno))
 
 
     def initialize_matrix(self):
@@ -56,18 +62,20 @@ class movie_predictor:
 
     def find_n_similar_neighbours(self):
         row_count = 0
+        user_to_simval = {}
+
         self.user_rat_list = self.user_item_matrix[self.user_id]
 
         for row in self.user_item_matrix:
             row_count += 1 
             if row_count == self.user_id:
                 continue
-            sim = cosine_similarity(user_rat_list, row_count)
-            self.user_to_simval[row_count] = sim
+            sim = self.cosine_similarity(self.user_rat_list, row)
+            user_to_simval[row_count] = sim
         
-        n_to_sim_dict = []
-        sorted_x = sorted(self.user_to_simval.iteritems(), key=operator.itemgetter(1))           
-        top_n_neighbour_tuple = sorted_x[:self.n]
+        n_to_sim_dict = {}
+        sorted_dict = sorted(user_to_simval.iteritems(), key=operator.itemgetter(1), reverse=True)           
+        top_n_neighbour_tuple = sorted_dict[:self.n_size]
 
         for a,b in top_n_neighbour_tuple:
             n_to_sim_dict[a] = b
@@ -88,23 +96,29 @@ class movie_predictor:
     def cosine_similarity(self, guser_rate_list, nuser_rate_list):
         d1 = self.root_square_sum(guser_rate_list)
         d2 = self.root_square_sum(nuser_rate_list)
-        
-        n = self.numerator(guser_rate_list, nuser_rate_list)
+        d = 0.0       
+ 
+        n = self.cosine_sim_num(guser_rate_list, nuser_rate_list)
+        d = d1 * d2 
             
-        return n/float(d1*d2)
+        if d ==0 :
+            return 0           
+
+        return n/d
 
     def root_square_sum(self, user_rate_list):
+        tot_val = 0
         for rate in user_rate_list:
-            tot_rate += rate*rate
+            tot_val += rate*rate
 
-        return math.sqrt(tot_rate)  
+        return math.sqrt(tot_val)  
                 
-    def numerator(self, guser_rate_list, nuser_rate_list):
+    def cosine_sim_num(self, guser_rate_list, nuser_rate_list):
+        tot_val = 0
         for a in range(len(guser_rate_list)):
             if a == self.item_id:
                 continue
             tot_val += guser_rate_list[a] * nuser_rate_list[a]
-            
         return tot_val
 
 
